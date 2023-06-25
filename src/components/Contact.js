264b65d088ec90a61ae7bc94cbf09c89acba6ff7
Aditya <mailforcoding86@gmail.com>
2023-06-25 17:33:20 +0530

    Third commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useRef, useEffect } from 'react';
+// import Marker from './Marker';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWRpdHlhc2hpcm9ka2FyMSIsImEiOiJjbGo4dWQ1bngxYTV4M2pxeXNsNzBuczc1In0.HsjE3CveeyVRoWN6gevRCw';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRpdHlhc2hpcm9ka2FyMSIsImEiOiJjbGo4dWQ1bngxYTV4M2pxeXNsNzBuczc1In0.HsjE3CveeyVRoWN6gevRCw';
 
 export default function Contact() {
   const mapContainer = useRef(null);
@@ -16,12 +17,26 @@ export default function Contact() {
       center: [72.8361797672783, 19.12339045436202],
       zoom: 13
     });
+    new mapboxgl.Marker({
+      color: 'red'
+    })
+      .setLngLat([72.8361797672783, 19.12339045436202])
+      .setPopup(new mapboxgl.Popup().setHTML("<h6>Sardar Patel Institute of Technology</h6><p>Bhavans Campus, Old D N Nagar, Munshi Nagar, Andheri West, Mumbai, Maharashtra 400058</p>"))
+      .addTo(map.current);
+
+      map.current.addControl(new mapboxgl.NavigationControl());
+
   });
+
+  // useEffect(() => {
+  //   new mapboxgl.Marker()
+  //     .setLngLat([72.8361797672783, 19.12339045436202])
+  //     .addTo(map.current);
+  // })
 
   return (
     <div>
-      <h2>Contact is here</h2>
-      <div ref={mapContainer} className="map-container" style={{width: '400px', height: '300px'}} />
+      <div ref={mapContainer} className="map-container" style={{width: '100%', height: '250px'}} />
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            <footer>
-                <p>This is the footer of my application.</p>
+        <>
+            <footer className="footer">
+                
             </footer>
-        </div>
+        </>
     )
 }
